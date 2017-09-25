a7b6f9499adc30d70dc765d559be35e3daac90c1
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-09-25 21:57:31 +0200

    added templates with GA enabled, js files etc

---
+++ js/app.js
@@ -0,0 +1,51 @@
+(function (self) {
+
+  //
+  //
+  // menu
+  //
+  //
+  // var isMenuVisible = false;
+
+  // var showMenu = function() {
+  //   // console.log("showing menu - begin: ", isMenuVisible);
+  //   $("#menu").show();
+  //   $("body").addClass("disabled-scroll");
+  //   isMenuVisible = true;
+  //   // console.log("showing menu - end: ", isMenuVisible);
+  // };
+
+  // var hideMenu = function() {
+  //   // console.log("hiding menu - begin: ", isMenuVisible);
+  //   $("#menu").hide();
+  //   $("body").removeClass("disabled-scroll");
+  //   isMenuVisible = false;
+  //   // console.log("hiding menu - end: ", isMenuVisible);
+  // };
+
+  // var toggleMenu = function() {
+  //   if (isMenuVisible) {
+  //     hideMenu();
+  //   }
+  //   else {
+  //     showMenu();
+  //   }
+  // };
+
+
+
+
+  //
+  //
+  // init
+  //
+  //
+  $(document).ready(function () {
+    // $(document).foundation();
+    // console.log("init menu");
+    // $("#menu-sign").click(function() {
+    //   toggleMenu();
+    // });
+
+  });
+})(this);
